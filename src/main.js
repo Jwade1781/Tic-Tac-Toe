34a4e10df1caee14a6ba0aaa9d6824ef54a2f104
Jwade1781 <jwade5219@gmail.com>
2022-05-26 14:53:56 -0400

    Added win conditional

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const DEBUG_PLAY = false;
+const DEBUG_PLAY = true;
 let gameController;
 
 const gameBoard = (() => {
@@ -6,28 +6,28 @@ const gameBoard = (() => {
     const BOARD_DIV = document.createElement("div");
     BOARD_DIV.id = "gameBoard";
 
-    const boxCreate = () => {
+    const boxCreate = (boxNum) => {
         const INNER_TEXT = document.createElement("h1");
         const BOX_ELEMENT = document.createElement("div");
         INNER_TEXT.classList.add("boxHeader");
         BOX_ELEMENT.appendChild(INNER_TEXT);
 
         BOX_ELEMENT.classList.add("boxElement");
-        BOX_ELEMENT.addEventListener("click", () => { 
-            console.log(`Box clicked by : ${gameController.getPlayerTurn().getHuman()}\t${gameController.getPlayerTurn().getMoveChar()}`)
-            if (INNER_TEXT.childNodes.length === 0){
+        BOX_ELEMENT.addEventListener("click", () => {
+            //console.log(`Box clicked by : ${gameController.getPlayerTurn().getHuman()}\t${gameController.getPlayerTurn().getMoveChar()}`)
+            if (INNER_TEXT.childNodes.length === 0) {
                 INNER_TEXT.textContent = gameController.getPlayerTurn().getMoveChar();
+                //console.log(`Set tile on box ${boxNum} : ${gameController.getPlayerTurn().getMoveChar()}`)
+                gameController.getPlayerTurn().setMove(boxNum);
                 gameController.changeTurn();
             }
-            console.log(`Set tile to : ${gameController.getPlayerTurn().getMoveChar()}`)
-            
         });
         return BOX_ELEMENT;
     }
 
     // Add all boxes into the grid
     for (let i = 0; i < TOTAL_GRIDS; i++) {
-        BOARD_DIV.appendChild(boxCreate());
+        BOARD_DIV.appendChild(boxCreate(i));
     }
 
     return BOARD_DIV;
@@ -60,12 +60,16 @@ class player {
     constructor(human, moveChar) {
         this.human = human;
         this.moveChar = moveChar;
+        this.moves = [];
     }
 
     setMoveChar(moveChar) { this.moveChar = moveChar; }
     setHuman(human) { this.human = human; }
+    setMove(move){this.moves.push(move);}
+
     getMoveChar() { return this.moveChar; }
     getHuman() { return this.human; }
+    getMoves(){return this.moves;}
 }
 
 class Players {
@@ -106,36 +110,70 @@ class GameController {
         console.log(this.players.printPlayers());
     }
 
-    getPlayerTurn = () => {return this.playerTurn}
+    getPlayerTurn = () => { return this.playerTurn }
     toggleDisplay = () => { this.ticTacToe.toggleDisplay(); }
-    makeTurn = () => {}
+    makeTurn = () => { }
     startGame = () => {
         this.ANNOUNCEMENT_HEADER.textContent = "Player 1's Turn";
         this.playerTurn = this.players.getPlayer1();
         this.toggleDisplay();
     }
-    
-    changeTurn = () => { 
-        
-        if (this.playerTurn === this.players.getPlayer1()) {
-            this.playerTurn = this.players.getPlayer2();
-            this.ANNOUNCEMENT_HEADER.textContent = "Player 2's Turn";
+
+    checkWin = () => {
+        const BOXES = document.querySelectorAll(".boxElement");
+        let currentPlayerMoves = this.playerTurn.getMoves().sort();
+        const WIN_CONDITIONS = [
+            [0,1,2], [3,4,5], [6.7,8], // Horizontal
+            [0,3,6], [1,4,7], [2,5,8], // Vertical
+            [0,4,8], [2,4,6]           // Diagnol
+        ]
+
+        for (let i = 0; i < currentPlayerMoves.length; i++){
+            let currentCheck = [];
+            for (let j = 0; j < 3; j++){
+                if (currentPlayerMoves[i+j] !== undefined) 
+                    currentCheck.push(currentPlayerMoves[i+j]);
+            }
+
+            for (let j = 0; j < WIN_CONDITIONS.length; j++){
+                const equals = (a, b) => JSON.stringify(a) === JSON.stringify(b);
+                if (equals(currentCheck, WIN_CONDITIONS[j])){
+                    console.log("Winner!")
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    changeTurn = () => {
+        if (!(this.checkWin())) {
+            if (this.playerTurn === this.players.getPlayer1()) {
+                this.playerTurn = this.players.getPlayer2();
+                this.ANNOUNCEMENT_HEADER.textContent = "Player 2's Turn";
+            }
+
+            else {
+                this.playerTurn = this.players.getPlayer1();
+                this.ANNOUNCEMENT_HEADER.textContent = "Player 1's Turn";
+            }
         }
 
         else {
-            this.playerTurn = this.players.getPlayer1(); 
-            this.ANNOUNCEMENT_HEADER.textContent = "Player 1's Turn";
+            console.log(this.playerTurn.getMoveChar() + " Wins!");
         }
     }
+
+
 }
 
 function main() {
     gameController = new GameController();
 
     if (DEBUG_PLAY) {
-        gameController.setPlayer(1, new player(true, "x"));
-        gameController.setPlayer(2, new player(false, "o"));
-        gameController.toggleDisplay();
+        gameController.setPlayer(1, true);
+        gameController.setPlayer(2, true);
+        gameController.startGame();
     }
 }
 
