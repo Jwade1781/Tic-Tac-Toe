8c8c574faa14124efe400128c2581debcacfa9a2
Jwade1781 <jwade5219@gmail.com>
2022-05-26 12:05:52 -0400

    Introduced turn switching and placement of chars

--- src/main.js
+++ src/main.js
@@ -10,11 +10,18 @@ const gameBoard = (() => {
         const INNER_TEXT = document.createElement("h1");
         const BOX_ELEMENT = document.createElement("div");
         INNER_TEXT.classList.add("boxHeader");
-        INNER_TEXT.textContent = "X";
         BOX_ELEMENT.appendChild(INNER_TEXT);
 
         BOX_ELEMENT.classList.add("boxElement");
-        BOX_ELEMENT.addEventListener("click", () => { console.log("Box clicked") });
+        BOX_ELEMENT.addEventListener("click", () => { 
+            console.log(`Box clicked by : ${gameController.getPlayerTurn().getHuman()}\t${gameController.getPlayerTurn().getMoveChar()}`)
+            if (INNER_TEXT.childNodes.length === 0){
+                INNER_TEXT.textContent = gameController.getPlayerTurn().getMoveChar();
+                gameController.changeTurn();
+            }
+            console.log(`Set tile to : ${gameController.getPlayerTurn().getMoveChar()}`)
+            
+        });
         return BOX_ELEMENT;
     }
 
@@ -25,6 +32,29 @@ const gameBoard = (() => {
 
     return BOARD_DIV;
 })();
+
+class TicTacToe {
+    constructor() {
+        this.GAME_BOARD = gameBoard;
+        this.GAME_DIV = document.querySelector("#gameDiv");
+        this.GAME_DIV.style.display = "none";
+        this.GAME_DIV.appendChild(this.GAME_BOARD);
+    }
+
+    setPlayers(players) { this.players = players; }
+    getPlayers() { return this.players; }
+    toggleDisplay() {
+        if (window.getComputedStyle(this.GAME_DIV).display === "none") {
+            this.GAME_DIV.style.display = "flex";
+            document.querySelector("#startingDiv").parentElement.style.display = "none"
+        }
+
+        else {
+            this.GAME_DIV.style.display = "none";
+            document.querySelector("#startingDiv").parentElement.style.display = "flex";
+        }
+    }
+}
 
 class player {
     constructor(human, moveChar) {
@@ -48,62 +78,64 @@ class Players {
     getPlayer2() { return this.player2; }
     setPlayer1(player1) { this.player1 = player1; }
     setPlayer2(player2) { this.player2 = player2; }
-    printPlayers(){console.log(`Player 1 -- Human : ${this.player1.getHuman()} -- Char : ${this.player1.getMoveChar()}\nPlayer 2 -- Human : ${this.player2.getHuman()} -- Char : ${this.player2.getMoveChar()}`)}
-}
-
-
-class TicTacToe {
-    constructor() {
-        this.GAME_BOARD = gameBoard;
-        this.GAME_DIV = document.querySelector("#gameDiv");
-        this.GAME_DIV.style.display = "none";
-        this.GAME_DIV.appendChild(this.GAME_BOARD);
-    }
-
-    setPlayers(players) { this.players = players; }
-    getPlayers() { return this.players; }
-    toggleDisplay() {
-        if (window.getComputedStyle(this.GAME_DIV).display === "none") {
-            this.GAME_DIV.style.display = "flex";
-            document.querySelector("#startingDiv").style.display = "none"
-        }
-
-        else {
-            this.GAME_DIV.style.display = "none";
-            document.querySelector("#startingDiv").style.display = "flex";
-        }
-    }
+    printPlayers() { console.log(`Player 1 -- Human : ${this.player1.getHuman()} -- Char : ${this.player1.getMoveChar()}\nPlayer 2 -- Human : ${this.player2.getHuman()} -- Char : ${this.player2.getMoveChar()}`) }
 }
 
 class GameController {
     constructor() {
         this.ticTacToe = new TicTacToe();
         this.players = new Players(new player(true, "X"), new player(true, "O")); // Players are human by default for easier debugging
+        this.ANNOUNCEMENT_HEADER = document.querySelector(".layer>h2");
     }
 
     setPlayer = (playerNum, human) => {
+        let selectText;
+        human ? selectText = "SELECTED HUMAN" : selectText = "SELECTED BOT";
         switch (playerNum) {
             case 1:
                 this.players.setPlayer1(new player(human, "X"));
+                document.querySelector("#player1Div>h4").textContent = `${selectText}`
                 break;
 
             case 2:
                 this.players.setPlayer2(new player(human, "O"));
+                document.querySelector("#player2Div>h4").textContent = `${selectText}`
                 break;
         }
 
         console.log(this.players.printPlayers());
     }
 
+    getPlayerTurn = () => {return this.playerTurn}
     toggleDisplay = () => { this.ticTacToe.toggleDisplay(); }
+    makeTurn = () => {}
+    startGame = () => {
+        this.ANNOUNCEMENT_HEADER.textContent = "Player 1's Turn";
+        this.playerTurn = this.players.getPlayer1();
+        this.toggleDisplay();
+    }
+    
+    changeTurn = () => { 
+        
+        if (this.playerTurn === this.players.getPlayer1()) {
+            this.playerTurn = this.players.getPlayer2();
+            this.ANNOUNCEMENT_HEADER.textContent = "Player 2's Turn";
+        }
+
+        else {
+            this.playerTurn = this.players.getPlayer1(); 
+            this.ANNOUNCEMENT_HEADER.textContent = "Player 1's Turn";
+        }
+    }
 }
 
 function main() {
     gameController = new GameController();
 
     if (DEBUG_PLAY) {
-        Players = new players[new player(true, "x"), new player(false, "o")];
-        TicTacToe(Players);
+        gameController.setPlayer(1, new player(true, "x"));
+        gameController.setPlayer(2, new player(false, "o"));
+        gameController.toggleDisplay();
     }
 }
 
